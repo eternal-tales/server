dd5b1c26e6b6b5887dbc442cc3203ac29f5e77ca
Changmin Kim <ckd29672@naver.com>
2024-02-01 16:01:32 +0900

    initialize project

---
+++ src/main/java/com/eternaltales/eternaltalesserver/EternaltalesserverApplication.java
@@ -0,0 +1,13 @@
+package com.eternaltales.eternaltalesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternaltalesserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EternaltalesserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eternaltales/eternaltalesserver/EternaltalesserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eternaltales.eternaltalesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EternaltalesserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
